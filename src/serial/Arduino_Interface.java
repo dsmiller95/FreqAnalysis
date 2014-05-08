d1fb5daa2696610833852045edd28692936a7491
dsmiller95 <dsmiller95@gmail.com>
2014-05-08 15:49:32 -0500

    Reorganized Arduino_Interface to reflect better interfacing methods

--- src/serial/Arduino_Interface.java
+++ src/serial/Arduino_Interface.java
@@ -6,15 +6,19 @@ public class Arduino_Interface {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void firePiston(){
-		
-	}
-	
-	public void isBoxInPosition(){
-		
-	}
-	
+	/**
+	 * Instructs the arduino to pluck the string, to maintain strong enough
+	 * amplitude
+	 */
 	public void pluckString(){
+		
+	}
+	
+	/**
+	 * Notifies the arduino of a frequency change
+	 * @param high true if the change is to a higher frequency, false otherwise
+	 */
+	public void notifyFreqChange(boolean high){
 		
 	}
 	
