f31b75470772186e254a4ffa45e3a9cda91158b9
dsmillerLAPTOP <millerds@msoe.edu>
2014-05-10 14:10:39 -0500

    filler commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,17 @@
 package main;
 
-import Audio.*;
 import serial.*;
 
 public class Main {
 	
-	static ArduinoInterface inter;
-	static AudioMonitor audio;
-	
 	public static class ArduinoListener implements Listener<SerialEvent>{
-		public void Activate(SerialEvent e) {
+		public void Activate(SerialEvent element) {
 			//Activate proper serial events
-			
 		}
 	}
 	
 	public static void main(String[] args){
-		inter = new ArduinoInterface(new ArduinoListener());
-		audio = new AudioMonitor();
-		inter.start();
+		ArduinoInterface m = new ArduinoInterface(new ArduinoListener());
+		m.start();
 	}
 }
