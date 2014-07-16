26bd63150538655eb3c5bd6bb0c8df216651b4ad
dsmillerLAPTOP <millerds@msoe.edu>
2014-07-16 16:25:16 -0500

    Added functionality to the arduinoStub

--- src/serial/ArduinoStub.java
+++ src/serial/ArduinoStub.java
@@ -28,42 +28,67 @@ public class ArduinoStub extends PApplet implements ArduinoInterface {
 	}
 	
 	private Listener<SerialEvent> alert;
-	private long lastPlucked = millis(), lastPiston = 0;
-	private boolean pistonFire = false;
+	private long lastPlucked = 0, lastPiston = 0;
+	private boolean pistonFire = true;
 	
 	public ArduinoStub(Listener<SerialEvent> alert) {
 		this.alert = alert;
-		main.Main.print("Opening arduino stub");
 		StubHolder hold = new StubHolder(this);
 	}
 	
 	public void setup(){
-		main.Main.print("setting up");
 		size(500, 500);
 		background(255);
+		textSize(25);
+		noStroke();
 	}
 	
-	int clr = 0;
 	public void draw(){
-		background(clr);
-		clr = (clr + 1)%255;
-		main.Main.print("Drawing");
-		/*if(lastPlucked + 1000 > millis()){
-			fill(0, 0, 255 * ((millis() - lastPlucked) / 900));
-			rect(50, 50, 100, 50);
-			stroke(0);
-			text("Plucked", 55, 55);
+		background(255);
+		float tmp;
+		if(lastPlucked + 1000 > millis()){
+			tmp = (float) (255 * ((millis() - lastPlucked) / 1000.0));
+			fill(tmp, tmp, 255);
+			rect(50, 50, 103, 40);
+			fill(255);
+			text("Plucked", 55, 80);
 		}
 		if(lastPiston + 1000 > millis()){
+			tmp = (float) (255 * ((millis() - lastPiston) / 1000.0));
 			if(pistonFire){
-				fill(255 * ((millis() - lastPlucked) / 900), 0, 0);
+				fill(255, tmp, tmp);
 			}else{
-				fill(0, 255 * ((millis() - lastPlucked) / 900), 0);
+				fill(tmp, 255, tmp);
 			}
-			rect(200, 50, 100, 50);
-			stroke(0);
-			text("Piston", 55, 55);
-		}*/
+			rect(200, 50, 120, 40);
+			fill(255);
+			text("Continue", 205, 80);
+		}
+		
+		stroke(0);
+		if(mousePressed && isMouseOnButton()){
+			fill(150, 150, 150);
+		}else{
+			fill(200, 200, 200);
+		}
+		rect(50, 150, 170, 40, 10);
+		fill(0);
+		text("Object in pos", 55, 180);
+		noStroke();
+	}
+	
+	public void mousePressed(){
+		if(isMouseOnButton()){
+			main.Main.print("Object in position signal");
+			
+			//wait for buffer to fill
+			for(long t = System.currentTimeMillis(); t + 750 > System.currentTimeMillis(););
+			alert.Activate(new SerialEvent(true));
+		}
+	}
+	
+	private boolean isMouseOnButton(){
+		return (mouseX > 50 && mouseX < 220 && mouseY > 150 && mouseY < 190);
 	}
 	
 	@Override
